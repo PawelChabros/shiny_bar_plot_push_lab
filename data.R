7cd4c6a230684a7932601ac2201284105cf1c4c3
Stanisław Baranowsky <pawel.chabros@yahoo.pl>
2019-01-26 10:52:58 +0100

    rm

--- data.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv('Zeszyt1.csv', sep = ';') %>%
-  as_tibble() %>%
-  gather(typ, n, -Rok) %>%
-  mutate(plt = if_else(
-    typ %in% c('akademickie', 'zawodowe'),
-    'plt2',
-    'plt1'
-  ))
